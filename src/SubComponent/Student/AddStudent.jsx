81b2260f120c7cd780fb331618337195cfec4d70
Thongchai <ais_arm@hotmail.com>
2018-09-06 18:20:26 +0700

    add datatable

--- src/SubComponent/Student/AddStudent.jsx
+++ src/SubComponent/Student/AddStudent.jsx
@@ -1,175 +1,18 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
-import classNames from 'classnames';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import Icon from '@material-ui/core/Icon';
-import AddIcon from '@material-ui/icons/Add';
+import 'antd/dist/antd.css';
+import { Form, Icon, Input, Button } from 'antd';
+const FormItem = Form.Item;
 
-const styles = theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    width:300,
-    margin:'0 auto'
-  },
-  margin: {
-    margin: theme.spacing.unit,
-  },
-  withoutLabel: {
-    marginTop: theme.spacing.unit * 3,
-  },
-  textField: {
-    flexBasis: '100%',
-  },
-  button: {
-    margin: '0 auto',
-    marginTop: theme.spacing.unit,
-  },
-});
 
-const titleranges = [
-  {
-    value: 'นาย',
-    label: 'นาย',
-  },
-  {
-    value: 'นางสาว',
-    label: 'นางสาว',
-  },
-  {
-    value: 'เด็กชาย',
-    label: 'เด็กชาย',
-  },
-  {
-    value: 'เด็กหญิง',
-    label: 'เด็กหญิง',
-  },
-];
 
 class AddStudentComponent extends Component {
-  state = {
-    title: '',
-    firstname: '',
-    lastname: '',
-    class: '',
-    id: '',
-    password:'',
-  };
-
-  handlesubmit = (e)=>{
-    e.preventDefault();
-  }
-  handleChange = prop => event => {
-    this.setState({ [prop]: event.target.value });
-  };
-
-  handleMouseDownPassword = event => {
-    event.preventDefault();
-  };
-
-  handleClickShowPassword = () => {
-    this.setState(state => ({ showPassword: !state.showPassword }));
-  };
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <form className={classes.root}>
-        <TextField
-          select
-          id="Title"
-          label="คำนำหน้าชื่อ"
-          className={classNames(classes.margin, classes.textField)}
-          value={this.state.title}
-          onChange={this.handleChange('title')}
-          
-        >
-          {titleranges.map(option => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField
-          label="ชื่อ"
-          id="Firstname"
-          value={this.state.firstname}
-          className={classNames(classes.margin, classes.textField)}
-          onChange={this.handleChange('firstname')}
-        />
-           <TextField
-          label="นามสกุล"
-          id="Lastname"
-          value={this.state.lastname}
-          className={classNames(classes.margin, classes.textField)}
-          onChange={this.handleChange('lastname')}
-        />
-        <FormControl className={classNames(classes.margin, classes.withoutLabel,classes.textField)}>
+  render(){
+    return(
+      <div>
         
-          <Input
-            id="class"
-            value={this.state.class}
-            onChange={this.handleChange('class')}
-            startAdornment={<InputAdornment position="start">มัธยมศึกษาปีที่</InputAdornment>}
-          />
-          <FormHelperText id="class-helper-text">ตัวอย่าง.มัธยมศึกษาปีที่ 6/13</FormHelperText>
-        </FormControl>
-        <FormControl
-          className={classNames(classes.margin, classes.textField)}
-        >
-        <InputLabel htmlFor="adornment-id">รหัสนักเรียน(username)</InputLabel>
-          <Input
-            id="id"
-            value={this.state.id}
-            onChange={this.handleChange('id')}
-          />
-          <FormHelperText id="id-helper-text">ตัวอย่าง.310xx</FormHelperText>
-        </FormControl>
-        <FormControl className={classNames(classes.margin, classes.textField)}>
-          <InputLabel htmlFor="adornment-password">รหัสผ่าน(password)</InputLabel>
-          <Input
-            id="password"
-            type={this.state.showPassword ? 'text' : 'password'}
-            value={this.state.password}
-            onChange={this.handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="Toggle password visibility"
-                  onClick={this.handleClickShowPassword}
-                  onMouseDown={this.handleMouseDownPassword}
-                >
-                  {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <Tooltip title="เพิ่มข้อมูลนักเรียนเข้าไปในฐานข้อมูล"  placement="right">
-        <Button variant="extendedFab" color="primary" className={classes.button}>
-        เพิ่มข้อมูล
-      </Button>
-      </Tooltip>
-      <br/>
-      <p>
-        {this.state.firstname}
-      </p>
-      
-      </form>
-    );
+      </div>
+    )
   }
-
 }
 
-export default withStyles(styles)(AddStudentComponent)
+export default AddStudentComponent
--- src/SubComponent/Student/ChangeStudent.jsx
+++ src/SubComponent/Student/ChangeStudent.jsx
@@ -1,14 +1,291 @@
 import React, { Component } from 'react';
-import { DatePicker, Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Table, Input, InputNumber, Popconfirm, Form, message, Dropdown, Menu, Icon, Button } from 'antd';
 import 'antd/dist/antd.css';
 
-class ChangeStudentComponent extends Component{
-    render(){
-        return(
+const data = [];
+for (let i = 0; i < 100; i++) {
+    data.push({
+        key: i.toString(),
+        id: `0000${i}`,
+        firstname: `firstname ${i}`,
+        lastname: `lastname ${i}`,
+        class: `6/${i}`,
+        password: `password ${i}`
+    });
+}
+const FormItem = Form.Item;
+const Search = Input.Search;
+const EditableContext = React.createContext();
+
+const EditableRow = ({ form, index, ...props }) => (
+    <EditableContext.Provider value={form}>
+        <tr {...props} />
+    </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends Component {
+    getInput = () => {
+        if (this.props.inputType === 'number') {
+            return <InputNumber />;
+        }
+        return <Input />;
+    };
+
+    render() {
+        const {
+            editing,
+            dataIndex,
+            title,
+            inputType,
+            record,
+            index,
+            ...restProps
+        } = this.props;
+        return (
+            <EditableContext.Consumer>
+                {(form) => {
+                    const { getFieldDecorator } = form;
+                    return (
+                        <td {...restProps}>
+                            {editing ? (
+                                <FormItem style={{ margin: 0 }}>
+                                    {getFieldDecorator(dataIndex, {
+                                        rules: [{
+                                            required: true,
+                                            message: `Please Input ${title}!`,
+                                        }],
+                                        initialValue: record[dataIndex],
+                                    })(this.getInput())}
+                                </FormItem>
+                            ) : restProps.children}
+                        </td>
+                    );
+                }}
+            </EditableContext.Consumer>
+        );
+    }
+}
+
+class EditableTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data,
+            datasearch: [],
+            searchby: 'id',
+            searching: false,
+            editingKey: ''
+        };
+
+        this.columns = [
+            {
+                title: 'Id',
+                dataIndex: 'id',
+                width: '12%',
+                editable: true,
+            },
+            {
+                title: 'First name',
+                dataIndex: 'firstname',
+                width: '20%',
+                editable: true,
+            },
+            {
+                title: 'Last name',
+                dataIndex: 'lastname',
+                width: '20%',
+                editable: true,
+            },
+            {
+                title: 'Class',
+                dataIndex: 'class',
+                width: '10%',
+                editable: true,
+            },
+            {
+                title: 'Password',
+                dataIndex: 'password',
+                width: '20%',
+                editable: true,
+            },
+            {
+                title: 'Operation',
+                dataIndex: 'operation',
+                render: (text, record) => {
+                    const editable = this.isEditing(record);
+                    return (
+                        <div>
+                            {editable ? (
+                                <span>
+                                    <EditableContext.Consumer>
+                                        {form => (
+                                            <a
+                                                href="javascript:;"
+                                                onClick={() => this.save(form, record.key)}
+                                                style={{ marginRight: 8 }}
+                                            >
+                                                Save
+                          </a>
+                                        )}
+                                    </EditableContext.Consumer>
+                                    <Popconfirm
+                                        title="Sure to cancel?"
+                                        onConfirm={() => this.cancel(record.key)}
+                                    >
+                                        <a>Cancel</a>
+                                    </Popconfirm>
+                                </span>
+                            ) : (
+                                    <div>
+                                        <a onClick={() => this.edit(record.key)} style={{marginRight:10}} >Edit</a>
+                                        {
+                                            this.state.searching ? (this.state.datasearch.length >= 1
+                                            ? (
+                                                <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.key)}>
+                                                    <a href="javascript:;">Delete</a>
+                                                </Popconfirm>
+                                            ) : null)
+                                            :(this.state.data.length >= 1
+                                                ? (
+                                                    <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.key)}>
+                                                        <a href="javascript:;">Delete</a>
+                                                    </Popconfirm>
+                                                ) : null)
+                                            }
+                                    </div>
+
+
+                                )}
+                        </div>
+                    );
+                },
+            },
+        ];
+    }
+
+    isEditing = (record) => {
+        return record.key === this.state.editingKey;
+    };
+
+    edit(key) {
+        this.setState({ editingKey: key });
+    }
+
+    save(form, key) {
+        form.validateFields((error, row) => {
+            if (error) {
+                return;
+            }
+            const newData = [...this.state.data];
+            const index = newData.findIndex(item => key === item.key);
+            if (index > -1) {
+                const item = newData[index];
+                newData.splice(index, 1, {
+                    ...item,
+                    ...row,
+                });
+                this.setState({ data: newData, editingKey: '' });
+                console.log("ba");
+            } else {
+                console.log("aa");
+                newData.push(row);
+                this.setState({ data: newData, editingKey: '' });
+            }
+        });
+    }
+
+    cancel = () => {
+        this.setState({ editingKey: '' });
+    };
+
+    SearchHandle = (e) => {
+        this.setState({ searching: true });
+        let textsearch = e.target.value;
+        let newData;
+        if (this.state.searchby === 'id') {
+            newData = data.filter(a_data => a_data.id.indexOf(textsearch) > -1);
+        }
+        else if (this.state.searchby === 'firstname') {
+            newData = data.filter(a_data => a_data.firstname.indexOf(textsearch) > -1);
+        }
+        else if (this.state.searchby === 'lastname') {
+            newData = data.filter(a_data => a_data.lastname.indexOf(textsearch) > -1);
+        }
+        else if (this.state.searchby === 'class') {
+            newData = data.filter(a_data => a_data.class.indexOf(textsearch) > -1);
+        }
+        this.setState({ datasearch: newData });
+    }
+
+    handleMenuClick = (e) => {
+        message.info('Search by ' + e.key, 0.8);
+        this.setState({ searchby: e.key });
+    }
+
+
+
+    render() {
+        const components = {
+            body: {
+                row: EditableFormRow,
+                cell: EditableCell,
+            },
+        };
+
+        const columns = this.columns.map((col) => {
+            if (!col.editable) {
+                return col;
+            }
+            return {
+                ...col,
+                onCell: record => ({
+                    record,
+                    inputType: col.dataIndex === 'age' ? 'number' : 'text',
+                    dataIndex: col.dataIndex,
+                    title: col.title,
+                    editing: this.isEditing(record),
+                }),
+            };
+        });
+        const menu = (
+            <Menu onClick={this.handleMenuClick}>
+                <Menu.Item key="id">Id</Menu.Item>
+                <Menu.Item key="firstname">Firstname</Menu.Item>
+                <Menu.Item key="lastname">Lastname</Menu.Item>
+                <Menu.Item key="class">Class</Menu.Item>
+            </Menu>
+        );
+
+
+        return (
             <div>
-                change student
+                <div style={{ marginBottom: 10, textAlign: 'center' }}>
+                    <Dropdown overlay={menu}>
+                        <Button style={{ marginRight: 1 }}>
+                            Column <Icon type="down" />
+                        </Button>
+                    </Dropdown>
+                    <Search
+                        placeholder={"search by " + this.state.searchby}
+                        // onSearch={value => console.log(value)}
+                        onChange={this.SearchHandle}
+                        style={{ width: 300 }}
+                    />
+                </div>
+                <Table
+                    pagination={{ pageSize: 5 }}
+                    components={components}
+                    bordered
+                    dataSource={this.state.searching ? this.state.datasearch : this.state.data}
+                    columns={columns}
+                    rowClassName="editable-row"
+                />
             </div>
         );
     }
 }
-export default ChangeStudentComponent
+
+
+export default EditableTable
